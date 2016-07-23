5a0522d4df4d3a79fe723212bb7f15d6b07f66bd
Cruz Cast <abstractime@outlook.com>
2016-07-23 18:51:44 -0500

    Empty app created

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name puestoMagicoWebFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the puestoMagicoWebFrontApp
+ */
+angular.module('puestoMagicoWebFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
